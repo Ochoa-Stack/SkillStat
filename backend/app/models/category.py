71d67af4a8bfef6358f5ef1562c80c671ec919c2
Ochoa-Stack <eliaslucinoochoamalaga@gmail.com>
2026-06-06 22:42:16 -0600

    feat(models): implement base entities category, city and user

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -1,1 +1,9 @@
-﻿# category — SkillStat
+from app.extensions import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    skills = db.relationship('Skill', backref='category', lazy=True)
